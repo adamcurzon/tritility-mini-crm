90a5397e581fe830b288851c2590cd0574126c4b
acurzon <adam@curzon.org>
2023-11-08 10:37:06 +0000

    refactor(php): Move routes to web

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::resources([
-    'company' => CompanyController::class,
-    'employee' => EmployeeController::class,
-]);
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    'company' => CompanyController::class,
+    'employee' => EmployeeController::class,
+]);
